008e98ed0d8f1b6d057f543e6a67caedd24cbe10
BhargavMaru07 <bhargavhamir@gmail.com>
2025-07-10 14:42:11 +0530

    day3

---
+++ Day3/Mission/Mission.Entities/MissionDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Mission.Entities
+{
+    public class MissionDbContextFactory : IDesignTimeDbContextFactory<MissionDbContext>
+    {
+        public MissionDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<MissionDbContext>();
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=MissionApiDb;Username=postgres;Password=bhargav;");
+
+            return new MissionDbContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ Day3/Mission/Mission.Entities/ViewModels/ResponseResult.cs
@@ -0,0 +1,15 @@
+﻿namespace Mission.Entities.ViewModels
+{
+    public class ResponseResult
+    {
+        public object Data { get; set; }
+        public ResponseStatus Result { get; set; }
+        public string Message { get; set; }
+    }
+    public enum ResponseStatus
+    {
+        Error,
+        Success
+    }
+}
+
